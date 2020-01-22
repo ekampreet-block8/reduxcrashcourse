7d06549efcfa8e665edfcc928f2e432fc12c879d
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-22 18:01:05 +0530

    created store and added imports

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
