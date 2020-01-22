3a9a593f3ea153606431ab5c49903d803770424b
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-22 14:58:17 +0530

    Added postform component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
+import Postform from "./components/Postform";
 
 function App() {
   return (
     <div className="App">
+      <Postform />
+      <hr></hr>
       <Posts />
     </div>
   );
