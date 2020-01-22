92fb8388d7661cb2aa4b4fa23e2a21dd862b1345
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-22 17:47:43 +0530

    installed react r-redux redux-thunk added provider

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <Postform />
-      <hr></hr>
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Postform />
+        <hr></hr>
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
