e5270fbb59b022f6e8a9c40115639d7ccc65aa81
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-23 11:31:00 +0530

    mapStateToProps

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,7 +7,7 @@ class Posts extends Component {
     this.props.fetchPosts();
   }
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -22,4 +22,8 @@ class Posts extends Component {
   }
 }
 
-export default connect(null, { fetchPosts })(Posts);
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
