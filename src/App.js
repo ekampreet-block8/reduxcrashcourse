eaaff3a17c9720cf2661fe5987d2aa169975818c
Ekampreet Singh <ekampreet.singh@block8.com>
2020-01-23 10:50:21 +0530

    made some changes to remove error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
 import Postform from "./components/Postform";
 import { Provider } from "react-redux";
-
-const store = createStore(() => [], {}, applyMiddleware());
+import store from "./store";
+// const store = createStore(() => [], {}, applyMiddleware());
 
 function App() {
   return (
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,18 +1,18 @@
 import { FETCH_POST, NEW_POST } from "../actions/types";
 
-const initialState={
-    items:[]
-    item:
-}
+const initialState = {
+  items: [],
+  item: {}
+};
 
-export default function(state=initialState,action){
-    switch(action.type){
-        case FETCH_POST:
-            return {
-                ...state,
-                items:action.payload
-            }
-        default:
-            return state;
-    }
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_POST:
+      return {
+        ...state,
+        items: action.payload
+      };
+    default:
+      return state;
+  }
 }
